68ef88f7a7f2862a05004c2ece40a7cf0dbd925f
Nathan HANEN <hanen.nathan@gmail.com>
2022-04-30 09:23:34 +0200

    username is readonly in Factory

--- src/app/dao/Factory.ts
+++ src/app/dao/Factory.ts
@@ -2,7 +2,7 @@ import { Article } from './Article';
 import { DummyFactory } from './DummyFactory';
 
 export interface Factory {
-    userName: string;
+    readonly userName: string;
     login(userName: string): boolean;
     logout(): void;
     getArticles(): Article[];
