3965bdef0da99372ddeef221c995f311e6f9f77d
Maxime MEYER <meyer.maxime212@gmail.com>
2022-04-30 09:28:39 +0200

    detail-product

--- src/app/detail-product/detail-product.page.ts
+++ src/app/detail-product/detail-product.page.ts
@@ -9,38 +9,30 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class DetailProductPage implements OnInit {
 
-  id: string;
-  titre: string;
-  lienImg: string;
-  description: string;
-  prinxConseille: number;
-  score: number;
-  favoris: boolean;
+  //id: number;
+  //titre: string;
+  //lienImg: string;
+  //description: string;
+  //prinxConseille: number;
+  //score: number;
+  //favoris: boolean;
 
   constructor(private activatedRoute: ActivatedRoute) {
-    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    //this.id = this.activatedRoute.snapshot.paramMap.get('idArticle');
   }
 
   ngOnInit() {
-    //axios.post('127.0.0.1:9876/api/utilisateur/detail-product/{id}', {
-    //    titre: this.titre,
-    //    lienImg: this.lienImg,
-    //    description: this.description,
-    //    prixConseille: this.prinxConseille,
-    //    score: this.score,
-    //    favoris: this.favoris
-    //  }
-    //  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-    //).then(function(response) {
-    //  if(response.status===200){ // si il n'y a pas d'erreur
-    //    // on affiche les données de l'artocle
-    //  }
-    //  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-    //}).catch(function(error) { // si il y a une erreur
-    //  console.log(error);
-    //  alert('Impossible de récupérer les données pour l\'article' + id);
-    //});
-    console.log(this.id);
+    axios.get('127.0.0.1:9876/api/utilisateur/detail-product/{id}'
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    ).then(function(response) {
+      if(response.status===200){ // si il n'y a pas d'erreur
+        // on affiche les données de l'article
+      }
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    }).catch(function(error) { // si il y a une erreur
+      console.log(error);
+      alert('Impossible de récupérer les données pour l\'article' + 'id');
+    });
   }
 
 }
