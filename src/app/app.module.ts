73dc3d83011c55aefce5b72ab14a3688b51fce58
Nathan HANEN <hanen.nathan@gmail.com>
2022-04-30 09:11:05 +0200

    forgot AppRoutingModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot()],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
     bootstrap: [AppComponent],
 })
