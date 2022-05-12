a25b9860338f227ca71e39b4e21929b0b7474306
Maxime MEYER <meyer.maxime212@gmail.com>
2022-05-12 15:45:18 +0200

    AJout bouton retour page produit

--- src/app/detail-product/detail-product.page.ts
+++ src/app/detail-product/detail-product.page.ts
@@ -27,19 +27,6 @@ export class DetailProductPage implements OnInit {
   }
 
   async ngOnInit() {
-    //this.id=2;
-    //axios.get('127.0.0.1:9876/api/utilisateur/detail-product/'+this.id
-    //  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-    //).then(function(response) {
-    //  if(response.status===200){ // si il n'y a pas d'erreur
-    //    // on affiche les données de l'article
-    //  }
-    //  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-    //}).catch(function(error) { // si il y a une erreur
-    //  //console.log(error);
-    //  alert('Impossible de récupérer les données pour l\'article' + this.id);
-    //});
-    // const art = FactoryImpl.getArticleById(this.id);
     const context = this;
     let art = null;
     await axios.get('http://127.0.0.1:9876/api/produit/'+this.id).then(article => art = article.data);
